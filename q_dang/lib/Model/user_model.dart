f78faecff2281835326e702fe300f0105749207b
고민혁 <rhalsgur0086@gmail.com>
2023-06-24 17:37:58 +0900

    add Jsonserialization factory constructor

--- q_dang/lib/Model/user_model.dart
+++ q_dang/lib/Model/user_model.dart
@@ -1,8 +1,18 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'user_model.g.dart';
+
+@JsonSerializable()
 class User {
   final int id;
   final String name;
 
-  User.fromJson({required Map<String, dynamic> json})
-      : id = int.parse(json['id']),
-        name = json['nickname'];
+  User({required this.id, required this.name});
+
+  factory User.fromJson(Map<String, dynamic> json)=>
+    _$UserFromJson(json);
+
+  // User.fromJson({required Map<String, dynamic> json})
+  //     : id = int.parse(json['id']),
+  //       name = json['nickname'];
 }
